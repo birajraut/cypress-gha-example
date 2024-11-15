31c643e3a8d8e15ae1b6cd7e4da08a61b917668e
birajraut <birajraut777@gmail.com>
2024-11-15 16:35:06 +0545

    added cypress grep for individual test action

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-grep/src/plugin')(config);
       return config;
     },
     env: {
