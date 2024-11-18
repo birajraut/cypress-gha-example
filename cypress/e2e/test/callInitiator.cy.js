2be5aa320a4cfb8065c79893cac1e046fa9cacd2
birajraut <birajraut777@gmail.com>
2024-11-18 12:41:55 +0545

    working in single specs

---
+++ cypress/e2e/test/callInitiator.cy.js
@@ -0,0 +1,15 @@
+
+describe('Cloud Telephony Automation', () => {
+  
+    before(() => {
+      // Base URL is defined in cypress.config.js
+      cy.visit('https://www.google.com/');
+    });
+  
+    it('should login as test user and initiate the call @smoke', () => {
+      // Enter username
+      cy.log('Hello test');
+        
+    });
+  });
+  
